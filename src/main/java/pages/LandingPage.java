a16704fc3ec9d0bccade2850ff4f9ecae458bda3
Pavel Canayev <Pavel.Canayev@upc.ua>
2021-08-10 00:49:17 +0300

    olx test cases first draft

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -1,8 +1,13 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LandingPage extends ParentPage {
+    @FindBy(id = "topLoginLink")
+    private WebElement hrefMyProfile;
+
     public LandingPage(WebDriver webDriver) {
         super(webDriver);
     }
